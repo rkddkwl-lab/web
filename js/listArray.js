edaf595d17b9da705837776fa42d6245f22bf8e1
강 건 희 <kgh10299@naver.com>
2026-03-27 15:35:37 +0900

    0327

---
+++ js/listArray.js
@@ -0,0 +1,66 @@
+let listArray = [
+    {
+        pid:0,
+        pName:"Bonheur blend Coffee",
+        pDesc:"부드러운 산미와 은은한 단맛이 조화를 이루는 대표 블렌드 커피",
+        pPrice:4500,
+        pThumbnail:"signature1.jpg",
+        plike:false
+    },
+    {
+        pid:1,
+        pName:"Bonheur Latte",
+        pDesc:"깊은 에소프레소에 우유의 밀도를 살린 클래식 시그니처 라떼",
+        pPrice:4500,
+        pThumbnail:"signature2.jpg",
+        plike:false
+    },
+    {
+        pid:2,
+        pName:"Vanilla Bonheur Latte",
+        pDesc:"부드러운 우유에 은은한 바닐라 향이 더해진 달콤한 라떼",
+        pPrice:5500,
+        pThumbnail:"signature3.jpg",
+        plike:false
+    },
+    {
+        pid:3,
+        pName:"Bonheur Cold Brew",
+        pDesc:"저온 추출로 완성한 깔끔하고 깊은 풍미의 콜드브루",
+        pPrice:5500,
+        pThumbnail:"signature4.jpg",
+        plike:false
+    },
+    {
+        pid:4,
+        pName:"Honey Cream Latte",
+        pDesc:"꿀의 자연스러운 단맛과 크림의 부드러움이 어우러진 라떼",
+        pPrice:5800,
+        pThumbnail:"best1.jpg",
+        plike:false
+    },
+    {
+        pid:5,
+        pName:"Caramel Milk Coffee",
+        pDesc:"쌉싸름한 커피에 카라멜의 달콤함을 더한 균형 잡힌 달달 음료",
+        pPrice:5800,
+        pThumbnail:"best2.jpg",
+        plike:false
+    },
+    {
+        pid:6,
+        pName:"Bonheur Cream Mocha",
+        pDesc:"초콜릿과 커피, 크림이 어우러진 진한 달콤함의 베스트 메뉴",
+        pPrice:6000,
+        pThumbnail:"best3.jpg",
+        plike:false
+    },
+    {
+        pid:7,
+        pName:"Bonheur Milk Choco",
+        pDesc:"진한 초콜릿과 우유의 부드러운 조화로 완성된 논커피 음료",
+        pPrice:5500,
+        pThumbnail:"best4.jpg",
+        plike:false
+    },
+] 
