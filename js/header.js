b47ba645adae7ed4362887b55d1c89b3187eaa2c
강 건 희 <kgh10299@naver.com>
2026-04-01 12:50:56 +0900

    0401

--- js/header.js
+++ js/header.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded",()=>{
+function header(){
     // 메뉴버튼을 누르면 header가 나오는 기능
     const btnMenu = document.querySelector('.btn-menu');
     const menuSmartHidden = document.querySelector('.menu-smart-hidden');
@@ -20,4 +20,6 @@ document.addEventListener("DOMContentLoaded",()=>{
             span.parentElement.classList.toggle("on")
         })
     })
-})
+}
+
+    
